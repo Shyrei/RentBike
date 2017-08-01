6983d6a9f0d99182acac2f3e64fdacac7fd54090
Uladzimir <shyrei.uladzimir@gmail.com>
2017-08-01 18:25:07 +0300

    Initial commit

---
+++ Web/src/main/java/by/shyrei/rentbike/action/admin/bike/ActAction.java
@@ -0,0 +1,21 @@
+package by.shyrei.rentbike.action.admin.bike;
+
+import by.shyrei.rentbike.action.Action;
+import by.shyrei.rentbike.controller.Router;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Project RentBike
+ * Created on 01.08.2017.
+ * author Shyrei Uladzimir
+ */
+public class ActAction implements Action{
+    @Override
+    public Router execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return null;
+    }
+}
